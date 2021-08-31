ee4748a60e5bced67b355a509f9eeef7bdff6214
Rafael Oliveira <oliveirafael@icloud.com>
2021-08-31 16:30:35 -0300

    - organize the project; - Create NewsListTableViewController

--- news App/Controller/NewsListTableViewController.swift
+++ news App/Controller/NewsListTableViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class NewsListTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
