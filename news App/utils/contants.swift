db9bbe906c7e54c4d83b5c55bd9458bf5db43d32
Rafael Oliveira <oliveirafael@icloud.com>
2021-09-02 20:44:04 -0300

    -add splash screen

--- news App/utils/contants.swift
+++ news App/utils/contants.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 struct K {
-    static let urlPath = "https://newsapi.org/v2/top-headlines?country=br&apiKey=8df1c88a69ae47999e77ca99b49de0fc"
+    static let urlPath = ""
     static let reusableIdentifier = "reusableCell"
 }
